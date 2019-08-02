7294492483bb68ebb2864157aaa437275c050361
nerdlet <brndnyokabi@gmail.com>
2019-08-02 18:14:39 +0300

    Fix favicons

---
+++ src/js/static-files.js
@@ -0,0 +1,14 @@
+// copy empty robots.txt
+import "../robots.txt";
+
+// copy favicons
+import "../favicons/android-chrome-512x512.png";
+import "../favicons/android-chrome-192x192.png";
+import "../favicons/apple-touch-icon.png";
+import "../favicons/browserconfig.xml";
+import "../favicons/favicon-16x16.png";
+import "../favicons/favicon-32x32.png";
+import "../favicons/favicon.ico";
+import "../favicons/mstile-150x150.png";
+import "../favicons/safari-pinned-tab.svg";
+import "../favicons/site.webmanifest";
