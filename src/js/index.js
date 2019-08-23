7c26dbef3d09cb4f4a03306c96c974c08a2755d5
nerdlet <brndnyokabi@gmail.com>
2019-08-23 16:43:05 +0300

    Disable map embed on page scroll, add query to get zoomlevel + center

--- src/js/index.js
+++ src/js/index.js
@@ -15,6 +15,7 @@ import {timeFormatLocale, timeParse} from 'd3-time-format';
 import {median} from 'd3-array';
 
 const d3 = Object.assign({}, d3_Selection, d3_Hexbin);
+import querystring from 'querystring';
 
 import api from './feinstaub-api';
 import * as config from './config.js';
@@ -102,7 +103,10 @@ const panelIDs = {
 
 const div = d3.select("#sidebar").append("div").attr("id", "table").style("display", "none");
 
-const map = L.map('map', {zoomControl: true, minZoom: config.minZoom, maxZoom: config.maxZoom, doubleClickZoom: false});
+let coordsCenter = config.center;
+let zoomLevel = config.zoom;
+
+const map = L.map('map', {center: coordsCenter, zoom:zoomLevel, zoomControl: true, minZoom: config.minZoom, maxZoom: config.maxZoom, doubleClickZoom: false});
 
 const tiles = L.tileLayer(config.tiles, {
 	attribution: config.attribution,
@@ -112,33 +116,28 @@ const tiles = L.tileLayer(config.tiles, {
 
 new L.Hash(map);
 
-// define query object
-const query = {
-	no_overlay: "false"
-};
-// iife function to read query parameter and fill query object
-(function () {
-	let telem;
-	const search_values = location.search.replace('\?', '').split('&');
-	for (let i = 0; i < search_values.length; i++) {
-		telem = search_values[i].split('=');
-		query[telem[0]] = '';
-		if (typeof telem[1] != 'undefined') query[telem[0]] = telem[1];
+const query = querystring.parse(window.location.search.substring(1));
+if (query.center) {
+	//Coordinates are passed by query
+	// /?center={lat},lng&zoom={zoom}
+	const center = query.center.split(',').map(coord => parseFloat(coord));
+	coordsCenter = center;
+	if (query.zoom) {
+		zoomLevel = parseInt(query.zoom);
 	}
-})();
-
-// show betterplace overlay
-if (query.no_overlay === "false") d3.select("#betterplace").style("display", "inline-block");
-
-let coordsCenter = config.center;
-let zoomLevel = config.zoom;
-
-if (location.hash) {
+} else if (location.hash) {
+		// Coordinates are passed by hash path
+	  // /#{zoom}/{lat}/{lng}
 	const hash_params = location.hash.split("/");
 	coordsCenter = [hash_params[1], hash_params[2]];
 	zoomLevel = hash_params[0].substring(1);
 } else {
-	const hostname_parts = location.hostname.split(".");
+	//Visited city or country subdomain
+	//https://{city or country}.map.aq.sensors.africa
+	//Currently not working
+	//NOT SUPPORTED
+	const hostname = location.hostname;
+	const hostname_parts = hostname.split(".");
 	if (hostname_parts.length === 4) {
 		const place = hostname_parts[0].toLowerCase();
 		console.log(place);
@@ -147,10 +146,17 @@ if (location.hash) {
 			zoomLevel = 11;
 		}
 		if (typeof zooms[place] !== 'undefined' && zooms[place] !== null) zoomLevel = zooms[place];
+		zoomLevel = zooms[place];
 		console.log("Center: " + coordsCenter);
 		console.log("Zoom: " + zoomLevel);
 	}
 }
+
+// Check if embedded and disable zoom
+if (window != window.top) {
+	map.scrollWheelZoom.disable();
+}
+
 
 window.onload = function () {
 	//	HEXBINS
